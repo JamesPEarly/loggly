15db8dbad133b333108d953803481bed0af342d3
Jim Early <jim.early@gooduncle.com>
2019-02-06 12:31:35 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,70 @@
+# loggly
+A Go (Golang) package for communicating with Loggly using user credentials
+
+## Setup
+In order to include this package in your Go application, you must import it as follows
+```
+import loggly "github.com/jamespearly/loggly"
+```
+
+## Initialization
+A connection to Loggly must be established before messages can be sent. Your program establishes its connection
+by first instantiating a client. The constructor requires a string parameter used as a tag for all messages sent by your application:
+```
+client = loggly.New('MyApplication')
+```
+
+## Sending Messages
+There are two functions provided for sending messages -- `Send` and `EchoSend`. Each function sends a valid
+log message to Loggly, but `EchoSend` additionally logs the message to the local console.
+
+Both functions require two string parameters. The first is the message `level`.
+The following is a list of valid levels accepted by Loggly (in order of decreasing severity):
+```
+"error"
+"warn"
+"info"
+"verbose"
+"debug"
+"silly"
+```
+The second string parameter is the `message text`. The following are valid example messages:
+```
+client.Send("debug", "This is a debug message");
+client.EchoSend("silly", "This is a silly message that is logged to the console");
+```
+
+## Example Program
+Here is an example Go program that opens a connection and sends various types of messages. It also shows
+a variety of errors:
+```
+// This package is used to test the Loggly package
+package main
+
+import (
+	"fmt"
+	loggly "github.com/jamespearly/loggly"
+)
+
+func main() {
+
+	var tag string
+	tag = "My-Go-Demo"
+
+	// Instantiate the client
+	client := loggly.New(tag)
+
+	// Valid EchoSend (message echoed to console and no error returned)
+	err := client.EchoSend("info", "Good morning!")
+	fmt.Println("err:", err)
+
+	// Valid Send (no error returned)
+	err = client.Send("error", "Good morning! No echo.")
+	fmt.Println("err:", err)
+
+	// Invalid EchoSend -- message level error
+	err = client.EchoSend("blah", "blah")
+	fmt.Println("err:", err)
+
+}
+```
---
+++ loggly.go
@@ -0,0 +1,125 @@
+// Package loggly provides an interface for sending messages to Loggly using
+// the users credentials supplied by an environment variable
+package loggly
+
+// Import packages
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+// Loggly API URL
+var logglyURL = "http://logs-01.loggly.com/inputs/"
+
+// Permitted levels
+var levelsAllowed = []string{"error", "warn", "info", "verbose", "debug", "silly"}
+
+// ClientType stores the URL and tag
+type ClientType struct {
+	URL string
+	Tag string
+}
+
+// messageType conforms to the JSON expected by Loggly
+type messageType struct {
+	Level     string `json:"level"`
+	Message   string `json:"message"`
+	Timestamp string `json:"timestamp"`
+}
+
+// New instantiates a new Loggly client using the
+// tag provided by the caller
+func New(tag string) *ClientType {
+
+	token := os.Getenv("LOGGLY_TOKEN")
+	newClient := &ClientType{}
+	newClient.URL = logglyURL + token + "/tag/" + tag
+	newClient.Tag = tag
+
+	return newClient
+}
+
+// Send constructs and sends a message to loggly.
+func (c *ClientType) Send(level string, message string) error {
+
+	// Validate the level
+	if !checkLevel(level) {
+		return errors.New("Invalid level: " + level)
+	}
+
+	// build the message
+	var msg messageType
+	msg.Level = level
+	msg.Message = message
+
+	// Timestamp the message
+	msg.Timestamp = time.Now().Truncate(time.Millisecond).UTC().String()
+
+	// Send message to loggly
+	c.sendToLoggly(msg)
+
+	return nil
+}
+
+// EchoSend sends a message to loggly and logs the
+// message to the console
+func (c *ClientType) EchoSend(level string, message string) error {
+
+	err := c.Send(level, message)
+	if err != nil {
+		return err
+	}
+
+	// log the message to the console
+	log.Println(level, message)
+
+	return nil
+}
+
+// non-exported functions
+
+// sendToLoggly sends message object contents to Loggly
+func (c *ClientType) sendToLoggly(msg messageType) error {
+
+	// Marshal message object to JSON
+	msgBytes, err := json.Marshal(msg)
+	if err != nil {
+		return errors.New("Could not create JSON payload")
+	}
+
+	// Construct the HTTP request with timeout
+	httpClient := &http.Client{
+		Timeout: 7 * time.Second}
+	request, err := http.NewRequest(http.MethodPost, c.URL, bytes.NewBuffer(msgBytes))
+	if err != nil {
+		return err
+	}
+
+	// Set the header attributes
+	request.Header.Add("Content-Type", "application/json")
+
+	_, err = httpClient.Do(request)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// This function validates a message level
+func checkLevel(level string) bool {
+
+	for _, testLevel := range levelsAllowed {
+		if testLevel == level {
+			// Match -- OK
+			return true
+		}
+	}
+
+	return false
+}
